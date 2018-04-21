21f26d60915ad2b875ac398a195f4f285c1bdc90
David Pollard <dsepollard@gmail.com>
2018-04-21 00:38:40 -0500

    first commit

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -20,6 +20,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+@IBAction func showAlert()
+{
+    let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
+    
+    let action = UIAlertAction(title: "Hi Asawa!", style: .default, handler: nil)
+    
+    alert.addAction(action)
+    
+    present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func knockKnock()
+    {
+        let alert = UIAlertController(title: "Knock Knock!", message: "Who's there!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Little old lady!", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
